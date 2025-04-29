86abd1330b8567e3f0f1a92f779148aabc9c1046
msitabheer20 <msitabheerdey@gmail.com>
2025-04-29 18:22:55 +0530

    fixed bug in server.js

--- config/database.js
+++ config/database.js
@@ -1,22 +1,21 @@
 import { Sequelize } from 'sequelize';
-       import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-       dotenv.config();
+dotenv.config();
 
-       const sequelize = new Sequelize(
-         process.env.DB_NAME,
-         process.env.DB_USER,
-         process.env.DB_PASSWORD,
-         {
-           host: process.env.DB_HOST,
-           dialect: 'mysql',
-           logging: false,
-         }
-       );
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
 
-       // Test connection
-       sequelize.authenticate()
-         .then(() => console.log('Database connected'))
-         .catch((err) => console.error('Database connection error:', err));
+sequelize.authenticate()
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.error('Database connection error:', err));
 
-       export default sequelize;
+export default sequelize;
--- models/task.js
+++ models/task.js
@@ -2,20 +2,20 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
 
 const Task = sequelize.define('Task', {
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-  completed: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    completed: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
 }, {
-  timestamps: true,
+    timestamps: true,
 });
 
 export default Task;
